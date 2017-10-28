d3f5e2bb89a8d9c03a0ed771e865297d493f5e57
monkeydunkey <bhushan.shashank93@gmail.com>
2017-10-28 16:13:03 -0700

    bug fixes

--- transferLearning_fineTuning.py
+++ transferLearning_fineTuning.py
@@ -3,6 +3,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential, Model
 from keras.layers import Input, Dropout, Flatten, Dense
+from keras.callbacks import ModelCheckpoint
+import keras
+from sklearn.metrics import roc_auc_score
+import numpy as np
 
 # path to the model weights files.
 top_model_weights_path = 'bottleneck_fc_model.h5'
@@ -13,8 +17,31 @@ train_data_dir = 'Data/train'
 validation_data_dir = 'Data/val'
 nb_train_samples = 1800
 nb_validation_samples = 200
-epochs = 50
+epochs = 10
 batch_size = 25
+
+class auc_roc_callback(keras.callbacks.Callback):
+    def __init__(self, val_data_generator, val_labels):
+        self.val_data_generator = val_data_generator
+        self.val_labels = val_labels
+        self.val_samples = val_labels.shape[0]
+
+    def on_train_begin(self, logs={}):
+        self.auc_history = []
+        self.loss = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred = self.model.predict_generator(self.val_data_generator, self.val_samples)
+        self.auc_history.append(roc_auc_score(self.val_labels, y_pred))
+        print 'AUC Score', self.auc_history[-1]
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        self.loss.append(logs.get('loss'))
+        return
 
 # build the VGG16 network
 input = Input(shape=(3, img_width, img_height),name = 'image_input')
@@ -33,14 +60,14 @@ top_model.add(Dense(1, activation='sigmoid'))
 # in order to successfully do fine-tuning
 top_model.load_weights(top_model_weights_path)
 
-# add the model on top of the convolutional base
-#model.add(top_model)
+for layer in model.layers:
+    layer.trainable = False
+
 mdl = Model(input= model.input, output= top_model(model.output))
 
 # set the first 25 layers (up to the last conv block)
 # to non-trainable (weights will not be updated)
-for layer in model.layers[:25]:
-    layer.trainable = False
+
 
 # compile the model with a SGD/momentum optimizer
 # and a very slow learning rate.
@@ -67,7 +94,22 @@ validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='binary',
+    shuffle=False)
+
+
+#Creating the callbacks
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+val_generator = val_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=False)
+
+val_labels = np.array([0]*2 + [1]*1)
+checkpointer = ModelCheckpoint(filepath="trainedmodel/weights.hdf5", verbose=1, save_best_only=True)
+auc_roc_hist = auc_roc_callback(val_generator, val_labels)
 
 # fine-tune the model
 mdl.fit_generator(
@@ -75,4 +117,6 @@ mdl.fit_generator(
     samples_per_epoch=nb_train_samples,
     nb_epoch=epochs,
     validation_data=validation_generator,
-    nb_val_samples=nb_validation_samples)
+    nb_val_samples=nb_validation_samples,
+    callbacks=[auc_roc_hist, checkpointer])
+print 'AUC Scores are', auc_roc_hist.auc_history
